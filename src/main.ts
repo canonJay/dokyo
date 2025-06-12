dbb5f8bc02af7e1edebdec6fcaa00568ef607905
cannonJay <andreylovecats@gmail.com>
2025-06-11 23:15:26 -0700

    Fix: CORS

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,55 @@
-import fastifyCookie from '@fastify/cookie'
-import { ValidationPipe } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
-import { NestFactory } from '@nestjs/core'
+import fastifyCookie from '@fastify/cookie';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
-} from '@nestjs/platform-fastify'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
-import { AppModule } from './app.module'
+} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-  
-  const configService = app.get(ConfigService)
+
+  const configService = app.get(ConfigService);
   app.register(fastifyCookie, {
-    secret: configService.get('COOKIE_SECRET') as string
+    secret: configService.get('COOKIE_SECRET') as string,
   });
 
   const config = new DocumentBuilder()
     .setTitle('API')
     .setDescription('API description')
     .setVersion('1.0')
-    .build()
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('swagger', app, document)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
 
-	app.enableCors({
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH', 'HEAD', 'CONNECT', 'TRACE'],
-		origin: ['*', 'http://localhost:5173/'],
-		credentials: true,
-		exposedHeaders: 'set-cookie'
-	})
+  app.enableCors({
+    methods: [
+      'GET',
+      'POST',
+      'PUT',
+      'DELETE',
+      'OPTIONS',
+      'PATCH',
+      'HEAD',
+      'CONNECT',
+      'TRACE',
+    ],
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    exposedHeaders: 'set-cookie',
+  });
 
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-  }))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
 
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 4200, '0.0.0.0');
