d67747cb0653cfcad1c17db852fe93fd31254973
cannonJay <andreylovecats@gmail.com>
2025-06-16 10:26:41 -0700

    Add: socket path

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -14,6 +14,7 @@ import { JoinChatDto, LeaveChatDto, SendMessageDto } from './dto/ws-message.dto'
 import { MessagesService } from './messages.service'
 
 @WebSocketGateway(4200, {
+  path: '/socket.io/',
   cors: {
     origin: true,
     credentials: true,
