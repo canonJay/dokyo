acbb67de748ab32a6da72228d3c1a4695ecdba80
Jay <100948836+canonJay@users.noreply.github.com>
2025-07-22 17:33:06 -0700

    add: s3 service

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -5,10 +5,11 @@ import { ProductsService } from 'src/products/products.service'
 import { OrdersController } from './orders.controller'
 import { OrdersService } from './orders.service'
 import { ProductImageService } from 'src/products/product-image.service'
+import { S3Service } from 'src/s3/s3.service'
 
 @Module({
   imports: [PaymentsModule],
   controllers: [OrdersController],
-  providers: [OrdersService, PrismaService, ProductsService, ProductImageService],
+  providers: [OrdersService, PrismaService, ProductsService, ProductImageService, S3Service],
 })
 export class OrdersModule {}
