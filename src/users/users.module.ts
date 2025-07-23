20989a2e3aa6d26e23544efb9dd8ecfb65e4a4f9
Jay <100948836+canonJay@users.noreply.github.com>
2025-07-22 18:41:07 -0700

    a

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from 'src/prisma.service'
 import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
+import { S3Service } from 'src/s3/s3.service'
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  providers: [UsersService, PrismaService, S3Service],
   exports: [UsersService],
 })
 export class UsersModule {}
